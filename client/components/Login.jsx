5f1707d07315de0d8f2f79994b1a153293866f69
wukaitao2006 <wukaitao2006@163.com>
2017-01-10 16:02:37 +0800

    测试跨域请求

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -3,12 +3,35 @@ import {Link} from 'react-router';
 import md5 from 'md5';
 //声明组件
 const Login = React.createClass({
+	getInitialState(){
+		return {
+			userName: 'admin3',
+			password: md5('5tgbSDFG')
+		};
+	},
+	login(){
+		const param = {
+			userName: this.state.userName,
+			password: this.state.password
+		};
+		fetch('/hmc_ghb_server/tob/tobLogin',{
+			method: 'post',
+			body: JSON.stringify(param)
+		}).then(response=>response.text())
+		.then(data=>{
+			console.log('登录成功!');
+			console.log(data);
+		}).catch(err=>{
+			console.log('登录失败!');
+		});
+	},
 	render(){
 		return(
 			<div>
 				This is Login.
 				<Link to="/login" activeClassName="current">Login</Link>
 				<Link to="/policymanage" activeClassName="current">PolicyManage</Link>
+				<button onClick={this.login}>登录</button>
 			</div>
 		)
 	}
