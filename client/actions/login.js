d78b7954fc319d9270e81fd88423583a5ed623ca
wukaitao2006 <wukaitao2006@163.com>
2017-01-12 09:33:40 +0800

    登录action

---
+++ client/actions/login.js
@@ -0,0 +1,44 @@
+﻿import * as types from './actionType.js';
+
+//导出所有方法
+//Login
+//action->reducer
+function requestLogin(param){
+	return {
+		type: types.Login,
+		status: 'beforeSend',
+		param
+	};
+}
+;
+function receiveLogin(param,data){
+	return {
+		type: types.Login,
+		status: 'success',
+		param,
+		data
+	};
+};
+function failLogin(param,err){
+	return {
+		type: types.Login,
+		status: 'error',
+		param,
+		err
+	};
+};
+//dispatch->action
+export function login(param){
+	return function(dispatch){
+		dispatch(requestLogin(param));
+		return fetch('../assets/json/policyLogin.json',{
+			method: 'post',
+			body: JSON.stringify(param)
+		}).then(response=>response.text())
+		.then(data=>{
+			dispatch(receiveLogin(param,JSON.parse(data)));
+		}).catch(err=>{
+			dispatch(failLogin(param,err));
+		});
+	};
+};
