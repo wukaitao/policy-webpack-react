71293e21bbc20f41ab7ede96cac0a53821fd3c01
wukaitao2006 <wukaitao2006@163.com>
2017-01-17 11:20:55 +0800

    完善样式和调整html结构

--- client/components/PointChoose.jsx
+++ client/components/PointChoose.jsx
@@ -1,14 +1,61 @@
 ﻿import React,{PropTypes} from 'react';
+import addons from 'react-addons';
 import {Link} from 'react-router';
 //声明组件
 const PointChoose = React.createClass({
-	propTypes: {
-		//组件的props安全
-	},
+	back(){},
+	chosenAll(p){},
 	render(){
+		const self = this;
+		const data = this.props.policyDetailData.data;
+		const classSet = addons.classSet;
+		const customNodeHeaderClass = classSet({
+			'chosenSome': this.chosenSome.bind(this,1),
+			'chosenAll': this.chosenAll1
+		});
+		const hospitalNodeHeaderClass = classSet({
+			'chosenSome': this.chosenSome.bind(this,5),
+			'chosenAll': this.chosenAll5
+		});
+		const classNodeHtml = <div>分类节点</div>;
+		const customNodeHtml = (
+			<dl>
+				<dt>
+					<header className={customNodeHeaderClass} onClick={this.chosenAll.bind(this,1)}>自定义标题</header>
+				</dt>
+				{
+					data.benefitList.map((item,index)=>{
+						let nodeClass = classSet({
+							'chosen': item.chosen
+						});
+						let resultHtml = item.nodeType==1 ? <dd key={index} className={nodeClass} onClick={self.choose.bind(this,item)}>{item.nodeTitle}</dd> : null;
+						return resultHtml;
+					})
+				}
+			</dl>
+		);
+		const hospitalNodeHtml = (
+			<dl>
+				<dt>
+					<header className={hospitalNodeHeaderClass} onClick={this.chosenAll.bind(this,5)}>医院</header>
+				</dt>
+				{
+					data.benefitList.map((item,index)=>{
+						let nodeClass = classSet({
+							'chosen': item.chosen
+						});
+						let resultHtml = item.nodeType==5 ? <dd key={index} className={nodeClass} onClick={self.choose.bind(this,item)}>{item.nodeTitle}</dd> : null;
+						return resultHtml;
+					})
+				}
+			</dl>
+		);
 		return (
-			<section className="main">
-				PointChoose.
+			<section className="main point-choose">
+				{classNodeHtml}
+				{customNodeHtml}
+				{hospitalNodeHtml}
+				<button className="btn btn-primary" onClick={this.back}>挑选完成</button>
 			</section>
 		)
 	}
--- client/components/PointDrag.jsx
+++ client/components/PointDrag.jsx
@@ -1,14 +1,45 @@
 ﻿import React,{PropTypes} from 'react';
 import {Link} from 'react-router';
+const dragObj = {};
 //声明组件
 const PointDrag = React.createClass({
-	propTypes: {
-		//组件的props安全
-	},
+	back(){},
+	pDragStart(event,obj){},
+	cDragStart(event,obj){},
+	pDragOver(event,obj){},
+	cDragOver(event,obj){},
+	pDrop(event,obj){},
+	cDrop(event,parent,obj){},
 	render(){
+		const self = this;
+		const data = this.props.policyDetailData.data;
+		const pointHtml = data.benefitList.map((item,index)=>{
+			const childrenPointHtml = item.children.map((subItem,subIndex)=>{
+				return(
+					<dd className="chosen" draggable="true" 
+						onDragstart={self.cDragStart.bind(this,$event,subItem)} 
+						onDragover={self.cDragOver.bind(this,$event,subItem)} 
+						onDrop={self.cDrop.bind(this,$event,item,subItem)}>
+						{subItem.nodeTitle}
+					</dd>
+				);
+			});
+			const resultHtml = (
+				<dl draggable="true" 
+					onDragstart={self.pDragStart.bind(this,$event,item)}
+					onDragover={self.pDragOver.bind(this,$event,item)} 
+					onDrop={self.pDrop.bind(this,$event,item)}>
+					<dt>
+						<header className="chosen">{item.nodeTitle}</header>
+					</dt>
+					{childrenPointHtml}
+				</dl>
+			);
+		});
 		return (
-			<section className="main">
-				PointDrag.
+			<section className="main point-drag">
+				{pointHtml}
+				<button className="btn btn-primary" onClick={this.back}>完成</button>
 			</section>
 		)
 	}
