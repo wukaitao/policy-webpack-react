c0c730c375d7903d11730e1f0ca332d28ad38850
wukaitao2006 <wukaitao2006@163.com>
2017-01-24 11:43:09 +0800

    弹窗完善

--- client/components/Popup.jsx
+++ client/components/Popup.jsx
@@ -1,8 +1,9 @@
 ﻿import React,{PropTypes} from 'react';
+import addons from 'react-addons';
 //声明组件
 const Popup = React.createClass({
 	componentWillUpdate(){
-		console.log('popup componentWillUpdate');
+		//console.log('popup componentWillUpdate');
 	},
 	openDialog(param){
 		this.props.dialogOpen(param);
@@ -21,6 +22,18 @@ const Popup = React.createClass({
 	render(){
 		console.log('popup render.');
 		const data = this.props.pageStatus;
+		const classSet = addons.classSet;
+		const dialogClass = classSet({
+			'hide': !data.dialog.show,
+			'dialog': true
+		});
+		const iToastClass = data.dialog.icon=='' ? 'icon-circle-cross' : data.dialog.icon;
+		const iConfirmClass = data.dialog.icon=='' ? 'icon-question' : data.dialog.icon;
+		const iAlertClass = data.dialog.icon=='' ? 'icon-notification' : data.dialog.icon;
+		const iTipsClass = data.dialog.icon=='' ? 'icon-info' : data.dialog.icon;
+		const iClass = data.dialog.type=='confirm' ? iConfirmClass : 
+					   data.dialog.type=='alert' ? iAlertClass : 
+					   data.dialog.type=='tips' ? iTipsClass : '';
 		const loadingHtml = (
 			<div className="cover">
 				<div className="loading">
@@ -54,9 +67,42 @@ const Popup = React.createClass({
 				</div>
 			</div>
 		);
+		const dialogHtml = (
+			<div className={dialogClass}>
+				<div className="dialog-wrap">
+					{data.dialog.type=='toast' ? (
+						<div className="toast-box">
+							<i className={iToastClass}></i>
+							<p dangerouslySetInnerHTML={{__html: data.dialog.message}}></p>
+						</div>
+					) : (
+						<div className="dialog-box" style={{width:data.dialog.style.width+'px'}}>
+							<div className="dialog-title">
+								<i onClick={this.closeDialog} className="icon-cross"></i>
+								{data.dialog.title}
+							</div>
+							<div className="dialog-content" style={{
+								height: data.dialog.style.maxHeight?'auto':data.dialog.style.height-66+'px'
+							}}>
+								<i className={iClass}></i>
+								<p dangerouslySetInnerHTML={{__html: data.dialog.message}} style={{
+									minHeight: data.dialog.style.maxHeight?data.dialog.style.height-76+'px':'100%',
+									maxHeight: data.dialog.style.maxHeight?data.dialog.style.maxHeight+'px':'auto'
+								}}></p>
+							</div>
+							<div className="dialog-toolbar">
+								<button onClick={this.confirmDialog} className="btn btn-primary">确定</button>
+								{data.dialog.type=='confirm' ? <button onClick={this.closeDialog} className="btn">取消</button> : null}
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+		);
 		return (
 			<footer className="foot popup">
 				{data&&data.isLoading ? loadingHtml : null}
+				{dialogHtml}
 			</footer>
 		)
 	}
