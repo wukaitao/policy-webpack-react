fc6d6fefd0d70b293cb9732c2db28823fe15ce23
wukaitao2006 <wukaitao2006@163.com>
2017-02-16 11:46:51 +0800

    文件名修改

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-﻿﻿import React from 'react';
+﻿import React from 'react';
 import ReactDom from 'react-dom';
 import {Router,Route,IndexRoute,Redirect,hashHistory,IndexRedirect} from 'react-router';
 import {Provider} from 'react-redux';
@@ -10,7 +10,7 @@ import PolicyEdit from './containers/PolicyEdit.js';
 import PointChoose from './containers/PointChoose.js';
 import PointDrag from './components/PointDrag.jsx';
 import PointManage from './containers/PointManage.js';
-import PointCreate from './containers/PointCreate.js';
+import PointEdit from './containers/PointEdit.js';
 import NotFound from './components/NotFound.jsx';
 require('./assets/css/main.scss');
 
@@ -49,10 +49,10 @@ const App = React.createClass({
 					<Route path='/pointchoose' component={PointChoose} onEnter={this.requireAuth}/>
 					<Route path='/pointdrag' component={PointDrag} onEnter={this.requireAuth}/>
 					<Route path='/pointmanage' component={PointManage} onEnter={this.requireAuth}/>
-					<Route path='/cateadd/:type' component={PointCreate} onEnter={this.requireAuth}/>
-					<Route path='/cateedit/:id' component={PointCreate} onEnter={this.requireAuth}/>
-					<Route path='/pointadd/:type/:parentId' component={PointCreate} onEnter={this.requireAuth}/>
-					<Route path='/pointedit/:pointId' component={PointCreate} onEnter={this.requireAuth}/>" +
+					<Route path='/cateadd/:type' component={PointEdit} onEnter={this.requireAuth}/>
+					<Route path='/cateedit/:id' component={PointEdit} onEnter={this.requireAuth}/>
+					<Route path='/pointadd/:type/:parentId' component={PointEdit} onEnter={this.requireAuth}/>
+					<Route path='/pointedit/:pointId' component={PointEdit} onEnter={this.requireAuth}/>" +
 					<Route path='/404' component={NotFound}/>
 					<Redirect from='*' to='/404'/>
 				</Route>
