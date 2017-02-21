8e6158660de27354766a4237ff42486aebeff4af
wukaitao2006 <wukaitao2006@163.com>
2017-02-21 11:07:30 +0800

    删除多余的actionType

--- client/actions/actionType.js
+++ client/actions/actionType.js
@@ -1,28 +1,15 @@
 ﻿//请求Action(Request)
 export const AllPointData = 'AllPointData';//查询所有节点数据
-
-export const ModifyPoint = 'ModifyPoint';//修改节点
-export const ModifyCate = 'ModifyCate';//修改分类
-export const SaveCreatePointData = 'SaveCreatePointData';//保存创建节点数据
-export const SaveModifyPointData = 'SaveModifyPointData';//保存修改节点数据
-export const SaveCreateCateData = 'SaveCreateCateData';//保存创建分类数据
-export const SaveModifyCateData = 'SaveModifyCateData';//保存修改分类数据
-
 export const PolicyListData = 'PolicyListData';//获取policy列表
 export const PolicyDetail = 'PolicyDetail';//policy详情
-export const PolicySave = 'PolicySave';//修改/创建policy
 export const PolicyInitChosen = 'PolicyInitChosen';//初始化节点选择
-export const PolicyFilterChosen = 'PolicyFilterChosen';//过滤节点选择
-export const PolicyRefreshOrder = 'PolicyRefreshOrder';//重新排序
 export const HospitalList = 'HospitalList';//获取医院列表
 export const SubmitPDF = 'SubmitPDF';//提交policy
-export const CreatePdf = 'CreatePdf';//生成pdf
 export const SendPdf = 'SendPdf';//发送pdf
 export const DialogOpen = 'DialogOpen';//打开弹窗
 export const DialogCancel = 'DialogCancel';//关闭弹窗
 export const LoadingOpen = 'LoadingOpen';//打开Loading蒙层
 export const LoadingCancel = 'LoadingCancel';//关闭Loading蒙层
-
 export const Login = 'Login';//登录
 export const Logout = 'Logout';//退出
 
