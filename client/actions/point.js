18514e0cde5408ba8b9496119ecf21fc0aa3f1fe
wukaitao2006 <wukaitao2006@163.com>
2017-02-23 12:26:14 +0800

    请求封装

--- client/actions/point.js
+++ client/actions/point.js
@@ -1,4 +1,5 @@
 ﻿import * as types from './actionType.js';
+import * as common from './common.js';
 import {loadingOpen,loadingCancel,dialogOpen,dialogCancel} from './popup.js';
 
 //导出所有方法
@@ -6,29 +7,24 @@ import {loadingOpen,loadingCancel,dialogOpen,dialogCancel} from './popup.js';
 //获取节点树
 export function queryAllPoint(param){
 	return (dispatch,getState)=>{
-		dispatch(loadingOpen());
-		return fetch('../assets/json/getTreeNode.json',{
-			method: 'get'
-		}).then(response=>response.text())
-		.then(data=>{
-			const result = JSON.parse(data);
-			if(result.statusCode==0){
+		common.baseDataService({
+			api: common.serverPath.getTreeNodeApi,
+			param: {method: 'get'},
+			showLoading: true,
+			dispatch
+		}).then((data)=>{
+			dispatch({
+				type: types.AllPointData,
+				data,
+				eventType: param.eventType,
+				keyword: param.keyword
+			});
+			setTimeout(()=>{
 				dispatch({
-					type: types.AllPointData,
-					data: result.data,
-					eventType: param.eventType,
-					keyword: param.keyword
+					type: types.ResetLetterList,
+					data: getState().allPointData
 				});
-				setTimeout(()=>{
-					dispatch({
-						type: types.ResetLetterList,
-						data: getState().allPointData
-					});
-				});
-			};
-			dispatch(loadingCancel());
-		}).catch(err=>{
-			dispatch(loadingCancel());
+			});
 		});
 	};
 };
@@ -87,26 +83,22 @@ export function changeNodeTitle(param){
 //保存分类/节点
 export function savePoint(param,successCallback=function(){},errorCallback=function(){}){
 	const isAdd = param.eventType=='pointadd'||param.eventType=='cateadd';
-	const urlApi = isAdd ?'../assets/json/templateNodeAdd.json':'../assets/json/templateNodeUpdate.json';
+	const urlApi = isAdd ? common.serverPath.templateNodeAddApi : common.serverPath.templateNodeUpdateApi;
 	delete param.eventType;
 	return (dispatch,getState)=>{
-		dispatch(loadingOpen());
-		return fetch(urlApi,{
-			method: 'get'
-		}).then(response=>response.text())
-		.then(data=>{
-			const result = JSON.parse(data);
-			if(result.statusCode==0){
-				dispatch(dialogOpen({
-					type: 'alert',
-					message: '保存成功.'
-				}));
-				successCallback.call(this);
-			};
-			dispatch(loadingCancel());
-		}).catch(err=>{
+		common.baseDataService({
+			api: common.serverPath.getTreeNodeApi,
+			param: {method: 'get'},
+			showLoading: true,
+			dispatch
+		}).then((data)=>{
+			dispatch(dialogOpen({
+				type: 'alert',
+				message: '保存成功.'
+			}));
+			successCallback.call(this);
+		},(err)=>{
 			errorCallback.call(this);
-			dispatch(loadingCancel());
 		});
 	};
 };
